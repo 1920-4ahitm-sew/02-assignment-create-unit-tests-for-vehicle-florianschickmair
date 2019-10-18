afb61bbb18e7cdb1f53a0362de3a06458292d6dc
florianschickmair <floschi100@gmail.com>
2019-10-18 12:42:47 +0200

    fixed mistake

--- vehiclest/src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
+++ vehiclest/src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
@@ -10,42 +10,51 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import static org.hamcrest.CoreMatchers.*;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
+
 public class VehicleEndpointIT {
-
-
     private Client client;
     private WebTarget target;
 
-
     @BeforeEach
-    public void initClient() {
+    public void initClient(){
         this.client = ClientBuilder.newClient();
         this.target = client.target("http://localhost:8080/vehicle/api/vehicle");
     }
-    @Test
-    public void crud() {
 
+    @Test
+    public void fetchVehicle(){
+        Response response = this.target.request(MediaType.TEXT_PLAIN).get();
+        assertThat(response.getStatus(), is(200));
+        String payload = response.readEntity(String.class);
+        System.out.println("payload = " + payload);
+    }
+
+    @Test
+    public void crud(){
         Response response = this.target.request(MediaType.APPLICATION_JSON).get();
         assertThat(response.getStatus(),is(200));
-        JsonArray allTodos = response.readEntity(JsonArray.class);
-        System.out.println("payload = " + allTodos);
-        assertThat(allTodos,not(empty()));
+        JsonArray payload = response.readEntity(JsonArray.class);
+        System.out.println("payload = " + payload);
+        assertThat(payload,not(empty()));
 
-        JsonObject vehicle = allTodos.getJsonObject(0);
+        JsonObject vehicle = payload.getJsonObject(0);
         assertThat(vehicle.getString("brand"),equalTo("Opel 42"));
-        assertThat(vehicle.getString("type"), startsWith("Commodore"));
-        // get with id
+        assertThat(vehicle.getString("type"),startsWith("Commodore"));
 
-        JsonObject dedicatedVehicle = this.target.path("43").request(MediaType.APPLICATION_JSON).get(JsonObject.class);
+        JsonObject dedicatedVehicle = this.target.path("43")
+                .request(MediaType.APPLICATION_JSON)
+                .get(JsonObject.class);
         assertThat(dedicatedVehicle.getString("brand"),containsString("43"));
         assertThat(dedicatedVehicle.getString("brand"),equalTo("Opel 43"));
 
-        Response deleteResponse = this.target.path("42").request(MediaType.APPLICATION_JSON).delete();
-        assertThat(deleteResponse.getStatus(),is(204)); // no content
+        Response deleteResponse = this.target.path("42")
+                .request(MediaType.APPLICATION_JSON).delete();
+        assertThat(deleteResponse.getStatus(),is(204));
     }
+
 }
